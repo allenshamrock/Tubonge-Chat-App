4ef89a7c2881f86ac2ec5c04dfccdc4d11b235ee
Allen <allenshamrock37@gmail.com>
2024-09-10 12:07:43 +0300

    {Feat}:Implemented the auth endpoints

---
+++ server/auth/__init__.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+from flask_restful import Api 
+from .routes import Login,Logout,UserToken
+
+auth_blueprint = Blueprint('auth_blueprint',__name__)
+api=Api(auth_blueprint)
+
+api.add_resource(Login,'/login')
+api.add_resource(Logout,'/logout')
+api.add_resource(UserToken,'/token')
+
+__all__=['auth_blueprint']
