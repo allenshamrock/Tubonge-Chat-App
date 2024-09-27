0b0529e5f90cd424e709753b9d4dd79efe9913a2
Allen <allenshamrock37@gmail.com>
2024-09-27 16:37:08 +0300

    {Enh}:Added the delete profile & edit profile feaures

---
+++ client/src/components/Sidebar/DeleteProfile.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import DeleteProfileModal from "components/modals/DeleteProfileModal";
+import Button from "components/common/Button";
+const DeleteProfile = () => {
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <>
+      <DeleteProfile
+        open={openModal}
+        onClose={() => {
+          setOpenModal(false);
+        }}
+      />
+      <Button
+        variant="danger"
+        onclick={() => {
+          setOpenModal(true);
+        }}
+      >
+        Delete profile
+      </Button>
+    </>
+  );
+};
+
+export default DeleteProfile;
---
+++ client/src/components/Sidebar/EditProfile.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import EditProfileModal from "components/modals/EditProfile";
+import Button from "components/common/Button";
+import useProfileHandlers from "hooks/useProfileHandlers";
+const EditProfile = () => {
+  const user = useSelector((state) => state.auth.user);
+  const [openModal, setOpenModal] = useState(false);
+
+  useProfileHandlers(user);
+
+  return (
+    <>
+      <EditProfileModal
+        user={user}
+        open={openModal}
+        close={() => {
+          setOpenModal(false);
+        }}
+      />
+      <Button
+        variant="danger"
+        onClick={() => {
+          setOpenModal(true);
+        }}
+      >
+        Edit profile
+      </Button>
+    </>
+  );
+};
+
+export default EditProfile;
